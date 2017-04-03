470b3d932d5565d1b70de99744be5fb7c790bfa1
alexey0308 <alexey.mipt@gmail.com>
2017-04-03 17:42:43 +0200

    change testdata

--- R/circtools/R/utils.R
+++ R/circtools/R/utils.R
@@ -40,21 +40,21 @@ testData <- function() {
   transNum <- 5
   exons <-  lapply(1:transNum,
                    function(x) {
-                     cbind(transcript_id = paste0("ENS000000", x),
+                     cbind(id = paste0("ENS000000", x),
                            exons[sample(exonsNum, sample(exonsNum, 1) +
                                           1), ])
                    })
   exons <- do.call(rbind, exons)
   res$exons <- exons
   # create expression levels
-  res$counts <- data.frame(ids = unique(exons$transcript_id),
+  res$counts <- data.frame(id = unique(exons$id),
                            count = round(2 ^ runif(
                              transNum, min = -10, max = 20
                            )))
   
   # primers
-  t1 <- exons$transcript_id[1]
-  e1 <- exons[exons$transcript_id == t1, ]
+  t1 <- exons$id[1]
+  e1 <- exons[exons$id == t1, ]
   e1 <- e1[order(e1$start), ]
   res$primers <- data.frame(id = paste0("circ", c(1, 1, 2)),
                             rbind(
