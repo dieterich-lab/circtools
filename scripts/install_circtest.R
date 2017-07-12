3afcb349541adc8b34219c885c60d95ec75ce43f
Tobias Jakobi <tobias.jakobi@med.uni-heidelberg.de>
2017-07-12 15:19:17 +0200

    Adding missing LICENSE header

--- scripts/install_circtest.R
+++ scripts/install_circtest.R
@@ -1,5 +1,20 @@
 #!/usr/bin/env Rscript
 
+# Copyright (C) 2017 Tobias Jakobi
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 # set mirrors
 source("https://bioconductor.statistik.tu-dortmund.de/biocLite.R")
 options(repos = c(CRAN = "https://cran.uni-muenster.de/"))
