b1529826c8cae0aa9cbd8be07c7664270f836146
Tobias Jakobi <tobias.jakobi@med.uni-heidelberg.de>
2017-07-12 14:24:21 +0200

    Issue #5: Adding Rscript to automatically install CircTest from GitHub

---
+++ scripts/install_circtest.R
@@ -0,0 +1,22 @@
+#!/usr/bin/env Rscript
+
+# set mirrors
+source("https://bioconductor.org/biocLite.R")
+options(repos = c(CRAN = "http://cran.rstudio.com"))
+
+# we need devtools as requirement for circtest
+pkgs <- c(
+  "devtools"
+
+)
+
+# check if devtools is already installed
+pkgs <- pkgs[!pkgs %in% installed.packages()[,1]]
+if (length(pkgs) > 0)
+  biocLite(pkgs)
+
+# load devtools library
+library(devtools)
+
+# install circtest from the dieterich lab github page from master branch
+install_github("dieterich-lab/CircTest", ref = "master")
