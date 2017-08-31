0b3646b23d514310c52d28e79666619ff9d0eb08
Tobias Jakobi <tobias.jakobi@med.uni-heidelberg.de>
2017-08-31 15:48:34 +0200

    Significant changes/updates; works with output of newest enrich module

--- scripts/circtools_enrich_visualization.R
+++ scripts/circtools_enrich_visualization.R
@@ -19,8 +19,8 @@
 
 suppressMessages(library(ggplot2))
 suppressMessages(library(ggrepel)) # beautifies text labels
-library(data.table)
-library(reshape2)
+suppressMessages(library(data.table))
+suppressMessages(library(reshape2))
 
 # may not be optimal, but we do not want warning for now
 options(warn = - 1)
@@ -32,54 +32,73 @@ options(echo = FALSE)
 args <- commandArgs(trailingOnly = TRUE)
 
 arg_data_file <- args[1] # path is string
+arg_pval <- as.numeric(args[2])
+arg_min_circRNAs <- as.integer(args[3]) # path is string
+arg_min_rbps <- as.integer(args[4]) # path is string
+arg_min_top_colors <- as.integer(args[5]) # path is string
+arg_output <- args[6] # path is string
+arg_experiment <- args[7] # path is string
+
+
+if (is.na(arg_output)){
+    arg_output <- "./output.pdf"
+}
+
+if (is.na(arg_experiment)){
+    arg_experiment <- "default"
+}
 
 message("Loading data file")
 rbp_data <- read.delim(arg_data_file, check.names=FALSE, header = F)
-colnames(rbp_data) <- c("RBP","Annotation","Chr","Start","Stop","Strand","PVal","Simulated","Observed")
 
+colnames(rbp_data) <- c("RBP", "Annotation", "chr", "start", "stop", "strand", "p_val_circular", "raw_count_circ_rna", "observed_input_peaks_circ_rna", "length_circ_rna", "length_normalized_count_circ_rna", "number_of_features_intersecting_circ", "circ_rna_confidence_interval_0.05", "p_val_linear", "raw_count_host_gene", "observed_input_peaks_host_gene", "length_host_gene_without_circ_rna", "length_normalized_count_host_gene", "number_of_features_intersecting_linear", "host_gene_confidence_interval_0.05", "distance_normalized_counts")
 
+# head(rbp_data)
 message("plotting data")
 
-# we use PDF and standard A4 page size
-pdf("test.pdf", height= 8.2, width=11.69 , title="circtools library analysis")
+rbp_data <- rbp_data[rbp_data$p_val_circular < arg_pval & rbp_data$p_val_linear > arg_pval, ]
 
+
+# we use PDF and standard A4 page size
+pdf(arg_output, height= 8.2, width=11.69 , title=paste("circtools RBP enrichment analysis - ",arg_experiment))
 
     # rbp plot
 
-    rbps <- table(rbp_data$RBP)
+    tmp <- data.frame(rbp_data$RBP)
+    colnames(tmp) <-c("RBP")
+    tmp$annotation <- rbp_data$Annotation
+    tmp <- unique(tmp)
+
+    rbps <- table(tmp$RBP)
     rbps <- sort(rbps, decreasing = TRUE)
     rbp_df <- data.frame(rbps)
     colnames(rbp_df) <-c("RBP","Frequency")
-    rbp_df <- rbp_df[rbp_df$Frequency>40,]
+    rbp_df <- rbp_df[rbp_df$Frequency>arg_min_circRNAs,]
 
-
-    rbp_simple_plot <- ggplot(data=rbp_df, aes(x=reorder(RBP, Frequency), y=Frequency)) +
+    rbp_simple_plot <- ggplot(data=rbp_df, aes(x=reorder(RBP, -Frequency), y=Frequency, fill=RBP)) +
                         geom_bar(stat="identity", colour="black") +
 
-                        # labs(   title = "Detected circular RNAs per million unique mapped reads",
-                        #         subtitle = "plotting circRNA predictions from DCC and uniquely mapped reads from STAR") +
-                        # labs(y = "Number of detected circular RNAs") +
-                        # labs(x = "Sequencing library") +
-                        # labs(fill = "Group") +
-                        # labs(caption = paste(   "based on data from ",
-                        #                         length(star_runs),
-                        #                         " mapped libraries\ncreated: ",
-                        #                         date(),
-                        #                         "",
-                        #                         sep="")) +
+                        labs(   title = "Number of circular RNAs per RBP",
+                                subtitle = paste("Counting circular RNAs (including different isoforms) with significantly enriched RBPs ( p <",
+                                arg_pval, ")")) +
+                        labs(y = "Number of circular RNAs") +
+                        labs(x = "RNA binding protein") +
+                        labs(caption = paste(   "based on data from ",
+                                                length(unique(rbp_data$RBP)),
+                                                " RBPs, showing RBPs with > ",
+                                                arg_min_circRNAs,
+                                                " circRNAs: ",
+                                                date(),
+                                                "",
+                                                sep="")) +
+
                         theme(  plot.title = element_text(lineheight=0.8,
                                 face=quote(bold)),
                                 legend.justification = c(1, 1),
-                                legend.position = c(0.25, 0.98),
+                                legend.position = "none",
                                 axis.text.x = element_text(angle = 90, hjust = 1, size=14)
 
                         )
-                        # geom_label_repel(   data=circle_ratio,
-                        #                     aes(x=name, y=num,
-                        #                     label=num),
-                        #                     box.padding = unit(0.0, "lines"),
-                        #                     point.padding = unit(0.0, "lines")
-                        #                 )
     print(rbp_simple_plot)
 
     # annotation plot
@@ -88,36 +107,32 @@ pdf("test.pdf", height= 8.2, width=11.69 , title="circtools library analysis")
     annotation <- sort(annotation, decreasing = TRUE)
     annotation_df <- data.frame(annotation)
     colnames(annotation_df) <-c("Annotation","Frequency")
-    annotation_df <- annotation_df[annotation_df$Frequency>10,]
+    annotation_df <- annotation_df[annotation_df$Frequency>arg_min_rbps,]
 
 
-    circ_simple_plot <- ggplot(data=annotation_df, aes(x=reorder(Annotation, Frequency), y=Frequency)) +
+    circ_simple_plot <- ggplot(data=annotation_df, aes(x=reorder(Annotation, -Frequency), y=Frequency, fill=Annotation)) +
                         geom_bar(stat="identity", colour="black") +
 
-                        # labs(   title = "Detected circular RNAs per million unique mapped reads",
-                        #         subtitle = "plotting circRNA predictions from DCC and uniquely mapped reads from STAR") +
-                        # labs(y = "Number of detected circular RNAs") +
-                        # labs(x = "Sequencing library") +
-                        # labs(fill = "Group") +
-                        # labs(caption = paste(   "based on data from ",
-                        #                         length(star_runs),
-                        #                         " mapped libraries\ncreated: ",
-                        #                         date(),
-                        #                         "",
-                        #                         sep="")) +
+                        labs(   title = "Number of RBP sites per circRNA",
+                                subtitle = paste("Counting circular RNAs (including different isoforms) with significantly enriched RBPs ( p <",
+                                arg_pval, ")")) +
+                        labs(y = "Number of enriched binding sites") +
+                        labs(x = "Circular RNA") +
+                        labs(caption = paste(   "based on data from ",
+                                                length(unique(rbp_data$Annotation)),
+                                                " circRNAs, showing circRNAs with > ",
+                                                arg_min_rbps,
+                                                " RPBs: ",
+                                                date(),
+                                                "",
+                                                sep="")) +
                         theme(  plot.title = element_text(lineheight=0.8,
                                 face=quote(bold)),
                                 legend.justification = c(1, 1),
-                                legend.position = c(0.25, 0.98),
+                                legend.position = "none",
                                 axis.text.x = element_text(angle = 90, hjust = 1, size=14)
 
                         )
-                        # geom_label_repel(   data=circle_ratio,
-                        #                     aes(x=name, y=num,
-                        #                     label=num),
-                        #                     box.padding = unit(0.0, "lines"),
-                        #                     point.padding = unit(0.0, "lines")
-                        #                 )
     print(circ_simple_plot)
 
 
@@ -136,43 +151,35 @@ pdf("test.pdf", height= 8.2, width=11.69 , title="circtools library analysis")
     }
     message(paste(counter,"circRNAs processed"))
     circRNA_df <- as.data.frame(do.call("rbind", tmp_list))
-    # colnames(circRNA_df) <- heading
-    # rownames(circRNA_df) <- (rownames(annotation))
     circRNA_df <- data.frame(rownames(annotation), circRNA_df)
     colnames(circRNA_df) <- c("CircRNA",heading)
-    circRNA_df_m <- melt(head(circRNA_df,10),id.vars = "CircRNA")
+    circRNA_df_m <- melt(head(circRNA_df,arg_min_top_colors),id.vars = "CircRNA")
     circRNA_df_m$CircRNA <- factor(circRNA_df_m$CircRNA, levels = circRNA_df_m$CircRNA)
 
-    print(circRNA_df_m)
-
-    # reorder(name, num)
     circ_simple_plot <- ggplot(data=circRNA_df_m, aes(x=CircRNA, y = value, fill = variable)) +
                         geom_bar(stat="identity") +
-
-                        # labs(   title = "Detected circular RNAs per million unique mapped reads",
-                        #         subtitle = "plotting circRNA predictions from DCC and uniquely mapped reads from STAR") +
-                        labs(y = "") +
-                        labs(x = "") +
-                        # labs(fill = "Group") +
-                        # labs(caption = paste(   "based on data from ",
-                        #                         length(star_runs),
-                        #                         " mapped libraries\ncreated: ",
-                        #                         date(),
-                        #                         "",
-                        #                         sep="")) +
+                        labs(   title = "Assignment of circRNAs to RPBs",
+                                subtitle = paste("plotting colour-coded RBPs to allow quick visulation of different groups per circRNA ( p <",
+                                            arg_pval, ")")) +
+                        labs(y = "Number of enriched binding sites") +
+                        labs(x = "RNA binding protein") +
+                        labs(caption = paste(   "based on data from ",
+                                                length(unique(rbp_data$Annotation)),
+                                                " circRNAs and ",
+                                                length(unique(rbp_data$RBP)),
+                                                " RBPs, showing top ",
+                                                arg_min_top_colors,
+                                                " RPBs: ",
+                                                date(),
+                                                "",
+                                                sep="")) +
                         theme(  plot.title = element_text(lineheight=0.8,
                                 face=quote(bold)),
                                 legend.justification = c(1, 1),
                                 legend.position = "none",
-                                axis.text.x = element_text(angle = 45, hjust = 1, size=14)
+                                axis.text.x = element_text(angle = 90, hjust = 1, size=14)
 
                         )
-                        # geom_label_repel(   data=circle_ratio,
-                        #                     aes(x=name, y=num,
-                        #                     label=num),
-                        #                     box.padding = unit(0.0, "lines"),
-                        #                     point.padding = unit(0.0, "lines")
-                        #                 )
     print(circ_simple_plot)
 
 
@@ -184,91 +191,3 @@ invisible(capture.output(dev.off()))
 invisible(capture.output(file.remove(list.files(pattern = "Rplots.pdf"))))
 
 message("Done")
-
-
-
-
-
-
-
-#     ######################### page one
-#
-#     # create data frame for ggplot2
-#     raw_counts <- data.frame(circ_counts_summed, linear_counts_summed, colors)
-#     colnames(raw_counts) <- c("circ","lin","group")
-#
-#     page_one <- ggplot( raw_counts, aes(x=circ, y=lin, color=as.factor(group), label=rownames(raw_counts))) +
-#                         geom_point() +
-#                         geom_label_repel(   data=raw_counts,
-#                                             aes(x=circ, y=lin,
-#                                             color=factor(group),
-#                                             label=rownames(raw_counts)),
-#                                             box.padding = unit(0.55, "lines"),
-#                                             point.padding = unit(0.5, "lines")
-#                                         ) +
-#                         labs(   title = "Circular vs. linear read counts throughout selected libraries",
-#                                 subtitle = "plotting non-normalized raw read counts from STAR") +
-#                         labs(x = "Circular RNA read count (log scale)") +
-#                         labs(y = "Linear RNA read count (log scale)") +
-#                         labs(colour = "Group") +
-#                         labs(caption = paste(   "based on data from ",
-#                                                 length(star_runs),
-#                                                 " mapped libraries\ncreated: ",
-#                                                 date(),
-#                                                 "",
-#                                                 sep="")) +
-#                         theme(  plot.title = element_text(lineheight=0.8,
-#                                 face=quote(bold)),
-#                                 # legend.justification = c(1, 1),
-#                                 # legend.position = c(0.95, 0.95)
-#                                 legend.justification = "top"
-#
-#                         ) +
-#                         scale_x_log10() +
-#                         scale_y_log10() +
-#                         annotation_logticks(sides = "trbl")
-#     print(page_one)
-#
-#     ######################### page two
-#
-#     # set number of circles
-#     number_of_circles <- apply(CircRNACount[, - c(1 : 3)], 2, function(x){length(which(x > 1))})
-#
-#     # reset names
-#     names(uniquely_mapped_reads) <- names(number_of_circles)
-#
-#     # create data frame for ggplot2
-#     circle_counts <- data.frame(uniquely_mapped_reads, number_of_circles, colors)
-#     colnames(circle_counts) <- c("unique","circles","group")
-#
-#     page_two <- ggplot( circle_counts, aes(x=unique, y=circles, color=as.factor(group), label=rownames(raw_counts))) +
-#                         geom_point() +
-#                         geom_label_repel(   data=circle_counts,
-#                                             aes(x=unique, y=circles,
-#                                             color=factor(group),
-#                                             label=rownames(raw_counts)),
-#                                             box.padding = unit(0.55, "lines"),
-#                                             point.padding = unit(0.5, "lines")
-#                                         ) +
-#                         labs(   title = "Number of mapped reads vs. number of detected circles per library",
-#                                 subtitle = "plotting uniquely mapped reads from STAR and circRNA predictions from DCC") +
-#                         labs(x = "Number of mapped reads (log scale)") +
-#                         labs(y = "Number of detected circles (log scale)") +
-#                         labs(colour = "Group") +
-#                         labs(caption = paste(   "based on data from ",
-#                                                 length(star_runs),
-#                                                 " mapped libraries\ncreated: ",
-#                                                 date(),
-#                                                 "",
-#                                                 sep="")) +
-#                         theme(  plot.title = element_text(lineheight=0.8,
-#                                 face=quote(bold)),
-#                                 # legend.justification = c(1, 1),
-#                                 # legend.position = c(0.95, 0.95)
-#                                 legend.justification = "top"
-#                         ) +
-#                         scale_x_log10() +
-#                         scale_y_log10() +
-#                         annotation_logticks(sides = "trbl")
-#     print(page_two)
-#
