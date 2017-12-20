0ba6f75cabfa834561c2a89a5cdae2b2a8561a74
alexey0308 <alexey.mipt@gmail.com>
2017-12-20 12:46:20 +0100

    left join!

--- scripts/exon_usage_circtools_wrapper.R
+++ scripts/exon_usage_circtools_wrapper.R
@@ -294,7 +294,7 @@ geneAttributes <- c("SYMBOL", "GENEID", "ENTREZID", "GENENAME")
 geneAnnotation <- ensembldb::select(
   annotationdb, topSplicedGenes$GeneID, geneAttributes, keytype = "GENEID")
 topSplicedGenesMartData <- merge(
-  topSplicedGenes, geneAnnotation, by.x = "GeneID", by.y = "GENEID")
+  topSplicedGenes, geneAnnotation, by.x = "GeneID", by.y = "GENEID", all.x = TRUE)
 
 # correct again for to 0-based positions
 dccDF<-CircCoordinates
