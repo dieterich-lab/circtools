b0b9f112f725fb1a98cf26541addfa9383443788
Tobias Jakobi <tobias.jakobi@med.uni-heidelberg.de>
2018-06-22 14:45:20 +0200

    Changing logo size

--- docs/conf.py
+++ docs/conf.py
@@ -82,7 +82,7 @@ pygments_style = 'sphinx'
 #
 html_theme = 'sphinx_rtd_theme'
 
-html_logo = 'img/circtools_200px.png'
+html_logo = 'img/circtools_150px.png'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
