2e13e1de5e12791da9f498c846126cc658c8649c
Tobias Jakobi <tobias.jakobi@med.uni-heidelberg.de>
2018-07-13 14:10:29 +0200

    Adding numpy to dependencies (error on fresh installs)

--- setup.py
+++ setup.py
@@ -187,6 +187,7 @@ setup(
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=[
+        'numpy>=1.14.5',
         'pybedtools>=0.7.10',
         'statsmodels>=0.8.0',
         'biopython >= 1.71'
