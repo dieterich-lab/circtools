b43e01ded7166bf9c689aae31d82be4e3037a065
Tobias Jakobi <tobias.jakobi@med.uni-heidelberg.de>
2018-07-25 15:10:28 +0200

    "Fixing" circtools detect call

--- tests/01_test_dcc.sh
+++ tests/01_test_dcc.sh
@@ -31,7 +31,7 @@ bunzip2 chr1.gtf.bz2
 cd 01_dcc/
 
 # execute DCC
-circtools detect @samplesheet -ss -T 2 -D -an ../chr1.gtf -A ../00_base/GRCh38_85.fa -R ../00_base/GRCh38_85_repeatmasker.gtf -B @bam_files.txt -M -Nr 2 2 -fg -G -t /tmp/ -F -L 20 -k -O output
+circtools detect -C "@samplesheet -ss -T 2 -D -an ../chr1.gtf -A ../00_base/GRCh38_85.fa -R ../00_base/GRCh38_85_repeatmasker.gtf -B @bam_files.txt -M -Nr 2 2 -fg -G -t /tmp/ -F -L 20 -k -O output"
 
 if [ "$OS" = "Mac" ]; then
   md5 output/*
