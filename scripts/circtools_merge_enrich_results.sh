dc35f88fdc7d845d3e55c4c9edff9a9d03130d7b
tjakobi <tobias.jakobi@med.uni-heidelberg.de>
2019-06-14 18:02:06 +0200

    Adding tested and fixed version of the enrich results merge script

---
+++ scripts/circtools_merge_enrich_results.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Copyright (C) 2018 Tobias Jakobi
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+# check if we have 2 arguments
+if [ ! $# == 4 ]; then
+  echo "Usage: $0 [Input path to enrich results] [Number of iterations (for regex)] [target dir e.g. /tmp/] [file name]"
+  exit
+fi
+
+ITER=$2
+
+awk -F '\t' '{print FILENAME"\t"$0}' $1/*.csv | sed 's/_.*$ITER_.*.csv//g' | grep -v circRNA_host_gene > $3/$4
